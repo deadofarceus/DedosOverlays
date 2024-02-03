ef211bbb3ca92ad1e08dac748e26ac4c5d236d2a
deadofarceus <nikilanger@yahoo.de>
2024-02-03 13:26:42 +0100

    fix: WHOOOOPS

--- src/types/WebsocketTypes.ts
+++ src/types/WebsocketTypes.ts
@@ -17,8 +17,8 @@ export class EloWebsocket {
     listenMode: boolean = false;
 
     constructor(summonerName: string, tag: string, key: string, queuetype: string, listenMode: string | null, callback: React.Dispatch<React.SetStateAction<Account>>) {
-        // this.wsAddress = `wss://modserver-dedo.glitch.me?name=${summonerName}&tag=${tag}`;
-        this.wsAddress = `ws://localhost:8080?name=${summonerName}&tag=${tag}`;
+        this.wsAddress = `wss://modserver-dedo.glitch.me?name=${summonerName}&tag=${tag}`;
+        // this.wsAddress = `ws://localhost:8080?name=${summonerName}&tag=${tag}`;
         this.ws = new WebSocket(this.wsAddress);
         this.summonerName = summonerName;
         this.tag = tag;
