5f789f13124028aa382508ca9e23d7fda0f5668d
deadofarceus <nikilanger@yahoo.de>
2024-03-13 19:38:55 +0100

    feat: add Basewebsocket

--- src/types/WebsocketTypes.ts
+++ src/types/WebsocketTypes.ts
@@ -3,33 +3,24 @@ import { DeathData } from "./DeathTypes";
 import { Game, Team } from "./FiveVFiveTypes";
 import { AbisZAccount, Account, Match, QUEUETYPES } from "./LeagueTypes";
 
-export class EloWebsocket {
+const GLOBALWSADRESS = "wss://modserver-dedo.glitch.me";
+// const GLOBALWSADRESS = "ws://localhost:8080";
 
-    summonerName: string;
-    tag: string;
-    key: string;
-    callback: React.Dispatch<React.SetStateAction<Account>>;
+export abstract class BaseWebSocket<T> {
+    callback: React.Dispatch<React.SetStateAction<T>>;
     ws: WebSocket;
-    wsAddress: string;
-    queueID: number;
     pingInterval!: NodeJS.Timeout;
-    gamefetchInterval!: NodeJS.Timeout;
-    interval: number = 10000;
-    ingame: boolean = false;
+    wsAddress: string;
 
-    constructor(summonerName: string, tag: string, key: string, queuetype: string, callback: React.Dispatch<React.SetStateAction<Account>>) {
-        this.wsAddress = `wss://modserver-dedo.glitch.me?name=${summonerName}&tag=${tag}`;
-        // this.wsAddress = `ws://localhost:8080?name=${summonerName}&tag=${tag}`;
-        this.ws = new WebSocket(this.wsAddress);
-        this.summonerName = summonerName;
-        this.tag = tag;
-        this.key = key;
-        this.queueID = QUEUETYPES.get(queuetype)!.queueId;
-
+    constructor(callback: React.Dispatch<React.SetStateAction<T>>, wsAddress: string) {
         this.callback = callback;
+        this.wsAddress = wsAddress;
+        this.ws = new WebSocket(this.wsAddress);
 
         this.setupWebSocket();
     }
+
+    abstract handleMessage(event: MessageEvent): void;
 
     setupWebSocket() {
         if (this.ws.readyState === this.ws.CLOSED) {
@@ -41,23 +32,57 @@ export class EloWebsocket {
         this.ws.onmessage = this.handleMessage;
     }
 
-    async fetchIngameData() {
-        await fetch("https://127.0.0.1:2999/liveclientdata/activeplayer", {
-            mode: "no-cors"
-        })
-            .then(response => {
-                // ingame
-                if (response) this.ingame = true;
-            })
-            .catch(() => {
-                // outgame
-                if (this.ingame) {
-                    //wechsel von ingame nach outgame make RIOTAPI Call
-                    console.log("GAME FINISHED");
-                    // this.sendAPIRequest();
-                }
-                this.ingame = false;
-            });
+    handleOpen = () => {
+        console.log("WebSocket connection established.");
+        this.setupPing();
+        while (this.ws.readyState !== this.ws.OPEN) { /* empty */ }
+    };
+
+    handleClose = (ev: CloseEvent) => {
+        if (ev.code !== 3500) {
+            console.log("WebSocket connection closed. Attempting to reconnect...");
+            clearInterval(this.pingInterval);
+            setTimeout(() => this.setupWebSocket(), 5000);
+        }
+    };
+
+    handleError = (error: Event) => {
+        console.error("WebSocket error occurred:", error);
+    };
+
+    setupPing() {
+        this.pingInterval = setInterval(() => this.ws.send("ping"), 60000);
+    }
+
+    sendEvent(modEvent: ModEvent) {
+        this.ws.send(JSON.stringify(modEvent));
+    }
+
+    checkUtilityEvent(message: string) {
+        if (message === "pong") return true;
+        if (message === "refresh") {
+            window.location.reload();
+            return true;
+        }
+    }
+}
+
+
+export class EloWebsocket extends BaseWebSocket<Account> {
+
+    summonerName: string;
+    tag: string;
+    key: string;
+    queueID: number;
+    ingame: boolean = false;
+
+    constructor(summonerName: string, tag: string, key: string, queuetype: string, callback: React.Dispatch<React.SetStateAction<Account>>) {
+        super(callback, `${GLOBALWSADRESS}?name=${summonerName}&tag=${tag}`);
+        this.summonerName = summonerName;
+        this.tag = tag;
+        this.key = key;
+        this.queueID = QUEUETYPES.get(queuetype)!.queueId;
+        this.setupWebSocket();
     }
 
     handleOpen = () => {
@@ -67,29 +92,14 @@ export class EloWebsocket {
         this.sendListenEvent();
     };
 
-    handleClose = (ev: CloseEvent) => {
-        if (ev.code !== 3500) {
-            console.log("WebSocket connection closed. Attempting to reconnect...");
-            clearInterval(this.pingInterval);
-            clearInterval(this.gamefetchInterval);
-            setTimeout(() => this.setupWebSocket(), 5000);
-        }
-    };
-
-    handleError = (error: Event) => {
-        console.error("WebSocket error occurred:", error);
-    };
-
     handleMessage = (event: MessageEvent) => {
         const message = event.data;
-        if (message === "pong") return;
-        if (message === "refresh") {
-            window.location.reload();
+        if (this.checkUtilityEvent(message)) {
             return;
         }
         const data = JSON.parse(message);
         const account = data.accounts[0] as Account;
-        console.log(account, Date.now());
+        console.log(account, Date.now().toLocaleString());
 
         account.lastThree = Array.from(
             new Set(account.lastThree.map((obj: Match) => JSON.stringify(obj)))
@@ -117,72 +127,29 @@ export class EloWebsocket {
         this.callback(account);
     };
 
-    setupPing() {
-        this.pingInterval = setInterval(() => this.ws.send("ping"), 60000);
-    }
-
     sendListenEvent() {
         const leagueLPEvent = new LeagueLPEvent([new Account("", "", this.summonerName, this.tag, this.queueID)], this.key);
         const modEvent = new ModEvent("league/listenAccount", leagueLPEvent);
-        this.ws.send(JSON.stringify(modEvent));
+        this.sendEvent(modEvent);
     }
 }
 
-export class DeathCounterWebsocket {
+export class DeathCounterWebsocket extends BaseWebSocket<DeathData> {
     id: string;
-    callback: React.Dispatch<React.SetStateAction<DeathData>>;
-    ws: WebSocket;
-    pingInterval!: NodeJS.Timeout;
-    wsAddress: string;
     currentStats: DeathData;
     timerCallback: React.Dispatch<React.SetStateAction<number>>;
 
     constructor(id: string, deathData: DeathData, callback: React.Dispatch<React.SetStateAction<DeathData>>, setBosstimer: React.Dispatch<React.SetStateAction<number>>) {
+        super(callback, `${GLOBALWSADRESS}?id=${id}`);
         this.id = id;
+        this.currentStats = deathData;
         this.callback = callback;
         this.timerCallback = setBosstimer;
-        this.wsAddress = `wss://modserver-dedo.glitch.me?id=${id}`;
-        // this.wsAddress = `ws://localhost:8080?id=${id}`;
-
-        this.ws = new WebSocket(this.wsAddress);
-
-        this.currentStats = deathData;
-        this.setupWebSocket();
     }
-
-    setupWebSocket() {
-        if (this.ws.readyState === this.ws.CLOSED) {
-            this.ws = new WebSocket(this.wsAddress);
-        }
-        this.ws.onopen = this.handleOpen;
-        this.ws.onclose = this.handleClose;
-        this.ws.onerror = this.handleError;
-        this.ws.onmessage = this.handleMessage;
-    }
-
-    handleOpen = () => {
-        console.log("WebSocket connection established.");
-        this.setupPing();
-        while (this.ws.readyState !== this.ws.OPEN) { /* empty */ }
-    };
-
-    handleClose = (ev: CloseEvent) => {
-        if (ev.code !== 3500) {
-            console.log("WebSocket connection closed. Attempting to reconnect...");
-            clearInterval(this.pingInterval);
-            setTimeout(() => this.setupWebSocket(), 5000);
-        }
-    };
-
-    handleError = (error: Event) => {
-        console.error("WebSocket error occurred:", error);
-    };
 
     handleMessage = (event: MessageEvent) => {
         const message = event.data;
-        if (message === "pong") return;
-        if (message === "refresh") {
-            window.location.reload();
+        if (this.checkUtilityEvent(message)) {
             return;
         }
         const data = JSON.parse(message);
@@ -197,21 +164,16 @@ export class DeathCounterWebsocket {
         this.callback(deathData);
     };
 
-    setupPing() {
-        this.pingInterval = setInterval(() => this.ws.send("ping"), 60000);
-    }
-
     sendDeaths(timerStart: number) {
-
-        const deathEvent = new DeathEvent(this.id, this.currentStats, timerStart!);
+        const deathEvent = new DeathEvent(this.id, this.currentStats, timerStart);
         const modEvent = new ModEvent("death/new", deathEvent);
-        this.ws.send(modEvent.tostring());
+        this.sendEvent(modEvent);
     }
 
     sendData() {
         const deathEvent = new DeathEvent(this.id, this.currentStats, undefined!);
         const modEvent = new ModEvent("death/new", deathEvent);
-        this.ws.send(modEvent.tostring());
+        this.sendEvent(modEvent);
     }
 
     changeDeaths(playerName: string, deathsNew: number) {
@@ -233,79 +195,28 @@ export class DeathCounterWebsocket {
     }
 }
 
-export class FiveVFiveWebsocket {
+export class FiveVFiveWebsocket extends BaseWebSocket<FiveVFiveEvent> {
     id: string;
-    callback: React.Dispatch<React.SetStateAction<FiveVFiveEvent>> | undefined;
-    ws: WebSocket;
-    pingInterval!: NodeJS.Timeout;
-    wsAddress: string;
     data: FiveVFiveEvent;
 
     constructor(id: string, callback: React.Dispatch<React.SetStateAction<FiveVFiveEvent>> | undefined) {
+        super(callback!, `${GLOBALWSADRESS}?id=${id}`);
         this.id = id;
-        this.callback = callback;
-        this.wsAddress = `wss://modserver-dedo.glitch.me?id=${id}`;
-        // this.wsAddress = `ws://localhost:8080?id=${id}`;
-
         this.data = new FiveVFiveEvent(id, new Team("Team 1", [], 0), new Team("Team 2", [], 0), "", "", "");
-        this.ws = new WebSocket(this.wsAddress);
-
-        this.setupWebSocket();
     }
-
-    setupWebSocket() {
-        if (this.ws.readyState === this.ws.CLOSED) {
-            this.ws = new WebSocket(this.wsAddress);
-        }
-        this.ws.onopen = this.handleOpen;
-        this.ws.onclose = this.handleClose;
-        this.ws.onerror = this.handleError;
-        this.ws.onmessage = this.handleMessage;
-    }
-
-    handleOpen = () => {
-        console.log("WebSocket connection established.");
-        this.setupPing();
-        while (this.ws.readyState !== this.ws.OPEN) { /* empty */ }
-    };
-
-    handleClose = (ev: CloseEvent) => {
-        if (ev.code !== 3500) {
-            console.log("WebSocket connection closed. Attempting to reconnect...");
-            clearInterval(this.pingInterval);
-            setTimeout(() => this.setupWebSocket(), 5000);
-        }
-    };
-
-    handleError = (error: Event) => {
-        console.error("WebSocket error occurred:", error);
-    };
 
     handleMessage = (event: MessageEvent) => {
         const message = event.data;
-        if (message === "pong") return;
-        if (message === "refresh") {
-            window.location.reload();
+        if (this.checkUtilityEvent(message)) {
             return;
         }
         const data = JSON.parse(message);
         const FiveVFiveData = data as FiveVFiveEvent;
-        console.log(data);
-
 
         if (this.callback) {
             this.callback(FiveVFiveData);
         }
     };
-
-    setupPing() {
-        this.pingInterval = setInterval(() => this.ws.send("ping"), 60000);
-    }
-
-    sendData() {
-        const modEvent = new ModEvent("fiveVfive", this.data);
-        this.ws.send(modEvent.tostring());
-    }
 
     gamePlayStatusChange(game: string, status: string, points: number) {
         this.data = { ...this.data, currentGame: "", bestof: "", standing: "" };
@@ -330,7 +241,7 @@ export class FiveVFiveWebsocket {
                 }
                 break;
         }
-        this.sendData();
+        this.sendEvent(new ModEvent("fiveVfive", this.data));
     }
 
     gameFormatChange(format: string) {
@@ -338,7 +249,7 @@ export class FiveVFiveWebsocket {
         if (format === "BestOf3" || format === "BestOf5") {
             this.data.standing = "0 : 0"
         }
-        this.sendData();
+        this.sendEvent(new ModEvent("fiveVfive", this.data));
     }
 
     removeGame(wonGames: Game[], game: string) {
@@ -349,71 +260,29 @@ export class FiveVFiveWebsocket {
 
     sendStanding(standing: string): void {
         this.data.standing = standing;
-        this.sendData();
+        this.sendEvent(new ModEvent("fiveVfive", this.data));
     }
 }
 
-export class AbisZWebsocket {
+export class AbisZWebsocket extends BaseWebSocket<AbisZAccount> {
     id: string;
-    callback: React.Dispatch<React.SetStateAction<AbisZAccount>>;
-    ws: WebSocket;
-    pingInterval!: NodeJS.Timeout;
-    wsAddress: string;
 
     constructor(id: string, callback: React.Dispatch<React.SetStateAction<AbisZAccount>>) {
+        super(callback, `${GLOBALWSADRESS}?id=${id}`);
         this.id = id;
-        this.callback = callback;
-        this.wsAddress = `wss://modserver-dedo.glitch.me?id=${id}`;
-        // this.wsAddress = `ws://localhost:8080?id=${id}`;
-
-        this.ws = new WebSocket(this.wsAddress);
-
-        this.setupWebSocket();
     }
-
-    setupWebSocket() {
-        if (this.ws.readyState === this.ws.CLOSED) {
-            this.ws = new WebSocket(this.wsAddress);
-        }
-        this.ws.onopen = this.handleOpen;
-        this.ws.onclose = this.handleClose;
-        this.ws.onerror = this.handleError;
-        this.ws.onmessage = this.handleMessage;
-    }
-
-    handleOpen = () => {
-        console.log("WebSocket connection established.");
-        this.setupPing();
-        while (this.ws.readyState !== this.ws.OPEN) { /* empty */ }
-    };
-
-    handleClose = (ev: CloseEvent) => {
-        if (ev.code !== 3500) {
-            console.log("WebSocket connection closed. Attempting to reconnect...");
-            clearInterval(this.pingInterval);
-            setTimeout(() => this.setupWebSocket(), 5000);
-        }
-    };
-
-    handleError = (error: Event) => {
-        console.error("WebSocket error occurred:", error);
-    };
 
     handleMessage = (event: MessageEvent) => {
         const message = event.data;
-        if (message === "pong") return;
-        if (message === "refresh") {
-            window.location.reload();
+        console.log(message);
+
+        if (this.checkUtilityEvent(message)) {
             return;
         }
         const data = JSON.parse(message);
         const account = data.account as AbisZAccount;
-        console.log(account);
+        console.log(Date.now().toLocaleString(), account);
 
         this.callback(account);
     };
-
-    setupPing() {
-        this.pingInterval = setInterval(() => this.ws.send("ping"), 60000);
-    }
 }
