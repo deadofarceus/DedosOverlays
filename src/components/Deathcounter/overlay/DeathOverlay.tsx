182436f89daa3aed6ca4ad61843e1ffa56dc4c8b
deadofarceus <nikilanger@yahoo.de>
2024-06-25 14:03:26 +0200

    feat: longer Boss names

--- src/components/Deathcounter/overlay/DeathOverlay.tsx
+++ src/components/Deathcounter/overlay/DeathOverlay.tsx
@@ -21,19 +21,41 @@ function DeathOverlay() {
   let total = 0;
   player.bosses.forEach((b) => (total += b.deaths.length - 1));
   const current = player.bosses[player.currentBoss];
+  const longestWord = findLongestWordLength(current.name);
   return (
     <Container className="deathOverlayCon">
       <Col className="centerC">
-        <Row className="w-100">
-          <span className="numOfDeaths blackOutline">💀 Gesamt: {total}</span>
-          <span className="numOfDeaths blackOutline">
-            💀 {current.name}: {current.deaths.length - 1}
-          </span>
-        </Row>
+        {longestWord < 11 && (
+          <Row className="w-100">
+            <span className="numOfDeaths blackOutline">💀 Gesamt: {total}</span>
+            <span className="numOfDeaths blackOutline">
+              💀 {current.name}: {current.deaths.length - 1}
+            </span>
+          </Row>
+        )}
+        {longestWord > 10 && (
+          <Col className="w-100 centerC">
+            <span className="numOfDeaths blackOutline">💀 Gesamt: {total}</span>
+            <span className="numOfDeaths w-100 blackOutline">
+              💀 {current.name}: {current.deaths.length - 1}
+            </span>
+          </Col>
+        )}
         <GraphOverlay player={player} tries={player.triesInGraph} />
       </Col>
     </Container>
   );
+}
+
+function findLongestWordLength(str: string) {
+  const words = str.split(" ");
+  let longestLength = 0;
+  for (const word of words) {
+    if (word.length > longestLength) {
+      longestLength = word.length;
+    }
+  }
+  return longestLength;
 }
 
 export default DeathOverlay;
