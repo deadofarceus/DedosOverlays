dfeed53b22690966f71ca7c603e25b1772196566
deadofarceus <nikilanger@yahoo.de>
2024-09-16 15:42:23 +0200

    feat: lp gain animation

--- src/components/league/EloInfo.tsx
+++ src/components/league/EloInfo.tsx
@@ -1,6 +1,7 @@
 import { Row, Col } from "react-bootstrap";
 import { AccountElo } from "../../types/LeagueTypes";
 import { useQuery } from "../../types/UsefulFunctions";
+import { useRef, useEffect, useState } from "react";
 
 function EloInfo({
   eloLP,
@@ -10,12 +11,51 @@ function EloInfo({
   gmBorder,
   challBorder,
 }: AccountElo) {
+  // Animations
+  const [displayedEloLP, setDisplayedEloLP] = useState(eloLP); // State to handle gradual increase
+  const previousEloLP = useRef(eloLP);
+
+  // Function to gradually increase the lpDiff
+  const animateEloLP = (startValue: number, endValue: number) => {
+    let currentValue = startValue;
+    const increment = (endValue - startValue) / 10; // Adjust this to control speed (30 frames)
+
+    const intervalId = setInterval(() => {
+      currentValue += increment;
+      setDisplayedEloLP(Math.round(currentValue)); // Update displayed value
+
+      const lpDiffElement = document.querySelector(".currentLP");
+      if (lpDiffElement) {
+        lpDiffElement.classList.add("bounce"); // Add bounce effect
+
+        // Remove bounce class after the animation
+        setTimeout(() => {
+          lpDiffElement.classList.remove("bounce");
+        }, 600); // Match the duration of your CSS bounce animation
+      }
+
+      if (Math.abs(currentValue - endValue) < 1) {
+        // Stop when close to the end value
+        clearInterval(intervalId);
+        setDisplayedEloLP(endValue); // Ensure the final value is exact
+      }
+    }, 100); // Adjust this for the timing of the steps
+  };
+
+  useEffect(() => {
+    if (previousEloLP.current !== eloLP) {
+      animateEloLP(previousEloLP.current, eloLP);
+      previousEloLP.current = eloLP; // Update reference to current lpDiff
+    }
+  }, [eloLP]);
+
+  // calculate alle displayed Werte
   let lpDisplay =
     eloDivision === "MASTER" ||
     eloDivision === "GRANDMASTER" ||
     eloDivision === "CHALLENGER"
-      ? eloLP + " LP"
-      : eloRank + " " + eloLP + " LP";
+      ? displayedEloLP + " LP"
+      : eloRank + " " + displayedEloLP + " LP";
   lpDisplay = eloDivision === "UNRANKED" ? "UNRANKED" : lpDisplay;
   const lpToday = lpDiff >= 0 ? `+${lpDiff} LP ↑` : `${lpDiff} LP ↓`;
   let border = undefined;
@@ -38,7 +78,9 @@ function EloInfo({
           className="eloimg eloAndLP"
         />
         {/* <p className={"currentLP eloAndLP pride " + prideflag}>{lpDisplay}</p> */}
-        <p className={"currentLP eloAndLP blackOutline"}>{lpDisplay}</p>
+        <p className={"currentLP eloAndLP blackOutline eloLPValue"}>
+          {lpDisplay}
+        </p>
         {/* {lpDisplay !== "UNRANKED" && (
           <p className="fatOutline currentLP eloAndLP prideshadow">
             {lpDisplay}
