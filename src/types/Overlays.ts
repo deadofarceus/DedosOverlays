76452ba2a14de3181d3ab45f874ca6fbf1848a37
deadofarceus <nikilanger@yahoo.de>
2024-09-21 21:39:14 +0200

    feat: add mapcover at home

--- src/types/Overlays.ts
+++ src/types/Overlays.ts
@@ -1,24 +1,34 @@
 export interface Overlay {
-    game: string;
-    imageURL: string;
-    hoverImageURL: string;
-    description: string;
-    url: string;
+  game: string;
+  imageURL: string;
+  hoverImageURL: string;
+  description: string;
+  url: string;
 }
 
 export const OVERLAYS: Overlay[] = [
-    {
-        game: "Elden Ring/other Soulslike",
-        imageURL: "/EldenRing.png",
-        hoverImageURL: "/EldenRingHover.png",
-        description: "Shows total and boss death with an nice little progress graph! Its not an overlay for the game (not a mod) but for your stream/when working with OBS. ",
-        url: "Deathcounter"
-    },
-    {
-        game: "League of Legends",
-        imageURL: "/LoL.png",
-        hoverImageURL: "/LoLHover.png",
-        description: "Elo Overlay that displays the last 5 games and LP gains today. And gives you flames if you have performed well! legacy Mode with old tier symbols included.",
-        url: "EloOverlay"
-    },
-]
+  {
+    game: "League of Legends",
+    imageURL: "/LoL.png",
+    hoverImageURL: "/LoLHover.png",
+    description:
+      "Elo Overlay that displays the last 5 games and LP gains today. And gives you flames if you have performed well! legacy Mode with old tier symbols included.",
+    url: "EloOverlay",
+  },
+  {
+    game: "Mapcover for League of Legends",
+    imageURL: "/Mapcover.png",
+    hoverImageURL: "/MapcoverHover.png",
+    description:
+      "Mapcover for League of Legends. It will always fit your ingame minimap size. Hides the minimap in multiple varriants and if you want add an Challenger screensaver ontop to distract your viewers.",
+    url: "Mapcover",
+  },
+  {
+    game: "Elden Ring/other Soulslike",
+    imageURL: "/EldenRing.png",
+    hoverImageURL: "/EldenRingHover.png",
+    description:
+      "Shows total and boss death with an nice little progress graph! Its not an overlay for the game (not a mod) but for your stream/when working with OBS. ",
+    url: "Deathcounter",
+  },
+];
