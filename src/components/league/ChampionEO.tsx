a8987b78bdd43fb01cb92968978a835fc3c87b4a
deadofarceus <nikilanger@yahoo.de>
2025-02-07 16:14:40 +0100

    fix: update patch

--- src/components/league/ChampionEO.tsx
+++ src/components/league/ChampionEO.tsx
@@ -33,7 +33,7 @@ function Champion({
   const imgsrc =
     championName === "null"
       ? "../../null.png"
-      : `https://ddragon.leagueoflegends.com/cdn/14.24.1/img/champion/${championName}.png`;
+      : `https://ddragon.leagueoflegends.com/cdn/15.2.1/img/champion/${championName}.png`;
   return (
     <div
       ref={championRef}
