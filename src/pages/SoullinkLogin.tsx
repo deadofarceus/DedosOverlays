8e9e6618a275e201bf565983eec5956adf5f1682
deadofarceus <nikilanger@yahoo.de>
2025-05-23 15:25:13 +0200

    fix: https

--- src/pages/SoullinkLogin.tsx
+++ src/pages/SoullinkLogin.tsx
@@ -4,7 +4,7 @@ const CLIENTID = "d8m29lejdzp24m8t4ahp7z7xxmlhe5";
 function SoullinkLogin() {
   document.body.className = "noOBS";
   //   const { channel } = useParams();
-  const twitchAuthUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${CLIENTID}&redirect_uri=http://${window.location.host}/Pokemon/Soullink/Kutcher&response_type=token&scope=user%3Aread%3Aemail`;
+  const twitchAuthUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${CLIENTID}&redirect_uri=https://${window.location.host}/Pokemon/Soullink/Kutcher&response_type=token&scope=user%3Aread%3Aemail`;
 
   return (
     <Container className="SoullinkContainer">
