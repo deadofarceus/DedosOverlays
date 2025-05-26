4a4872fd9fd12a676571b4c9c42edb28ea45e9cc
deadofarceus <nikilanger@yahoo.de>
2025-05-26 20:21:27 +0200

    fix: lol

--- src/pages/SoullinkLogin.tsx
+++ src/pages/SoullinkLogin.tsx
@@ -3,7 +3,7 @@ const CLIENTID = "d8m29lejdzp24m8t4ahp7z7xxmlhe5";
 
 function SoullinkLogin() {
   document.body.className = "noOBS";
-  const twitchAuthUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${CLIENTID}&redirect_uri=http://${window.location.host}/Pokemon/Soullink&response_type=token&scope=user%3Aread%3Aemail`;
+  const twitchAuthUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${CLIENTID}&redirect_uri=https://${window.location.host}/Pokemon/Soullink&response_type=token&scope=user%3Aread%3Aemail`;
 
   return (
     <Container className="SoullinkContainer centerC">
