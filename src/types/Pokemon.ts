f6376d80e5e402a3819aed0909355734eecf8e79
deadofarceus <nikilanger@yahoo.de>
2025-06-12 02:35:41 +0200

    feat: pokemon classes

--- src/types/Pokemon.ts
+++ src/types/Pokemon.ts
@@ -8,3 +8,28 @@ export interface PokemonEvent {
 export interface PokemonInfos {
   userName: string;
 }
+
+export interface Soullink {
+  id: string;
+  trainers: Trainer[];
+}
+
+export interface Trainer {
+  name: string;
+  pokemon: Pokemon[];
+}
+
+export class Pokemon {
+  id: string;
+  name: string;
+  deName: string;
+  image: string;
+  route: string;
+  constructor(id: string, name: string, deName: string, route: string) {
+    this.id = id;
+    this.name = name;
+    this.deName = deName;
+    this.image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+    this.route = route;
+  }
+}
