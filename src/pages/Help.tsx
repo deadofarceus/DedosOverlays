31942ef4043b66a67bfa421a67cb6704985d62f2
deadofarceus <nikilanger@yahoo.de>
2025-08-27 17:50:01 +0200

    fix: fixed q and a

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -13,9 +13,8 @@ function Help() {
               {1}. {FAQS[0].question}
             </h2>
             <p className="answers">
-              Yes, it is free to use. Check out the{" "}
-              <a href="/EloOverlay">tutorial</a>. Although it is free, I
-              appreciate any small donation:{" "}
+              Yes, it is free to use. Check out the <a href="/EloOverlay">tutorial</a>. Although it
+              is free, I appreciate any small donation:{" "}
               <a href="https://paypal.me/deadofarceus" aria-label="PayPal">
                 <img
                   height="36"
@@ -32,11 +31,10 @@ function Help() {
                   src="https://cdn.prod.website-files.com/5c14e387dab576fe667689cf/61e1116779fc0a9bd5bdbcc7_Frame%206.png"
                 />
               </a>{" "}
-              You will need a key to use the overlay. You can get the Elo
-              Overlay (and your key) by contacting me on{" "}
-              <a href="https://twitter.com/deadofarceus">Twitter</a>{" "}
-              @deadofarceus or via Twitch whisper also @deadofarceus. Gerne auch
-              auf Deutsch {":)"}
+              You will need a key to use the overlay. You can get the key by logging in with Twitch
+              in the Tutorial. If you have any problems, you can contact me on{" "}
+              <a href="https://twitter.com/deadofarceus">Twitter</a> @deadofarceus or via Twitch
+              whisper also @deadofarceus. Gerne auch auf Deutsch {":)"}
             </p>
           </div>
           <div className="faq">
@@ -50,8 +48,7 @@ function Help() {
               {3}. {FAQS[2].question}
             </h2>
             <p className="answers">
-              Just message me over{" "}
-              <a href="https://twitter.com/deadofarceus">Twitter</a>{" "}
+              Just message me over <a href="https://twitter.com/deadofarceus">Twitter</a>{" "}
               @deadofarceus so we can talk about your ideas.
             </p>
           </div>
