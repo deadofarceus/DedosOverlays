5c3ee62f2e414cf6de8daadc81fb2372340ee3d9
deadofarceus <nikilanger@yahoo.de>
2025-08-27 17:58:48 +0200

    fix: set Key

--- src/pages/EloOverlayTutorial.tsx
+++ src/pages/EloOverlayTutorial.tsx
@@ -70,11 +70,9 @@ function EloOverlayTutorial() {
         window.location.hash = "";
 
         fetch(`https://${GLOBALADDRESS}/getkey/LeagueOfLegends?token=${token}`)
-          .then((res) => res.text())
+          .then((res) => res.json())
           .then((data) => {
-            console.log(data);
-
-            // setKey(data.key);
+            setKey(data.key);
           })
           .catch((err) => console.log(err));
       } else {
@@ -124,29 +122,31 @@ function EloOverlayTutorial() {
                     id="dedoKey"
                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => setKey(e.target.value)}
                   />
-                  <div className="mt-2">
-                    <Button
-                      className="twitchLoginButton"
-                      href={`https://id.twitch.tv/oauth2/authorize?client_id=${CLIENTID}&redirect_uri=https://${window.location.host}/EloOverlay&response_type=token&scope=user%3Aread%3Aemail`}
-                      aria-label="Login with Twitch"
-                    >
-                      <span className="twitchIcon" aria-hidden>
-                        <svg
-                          width="20"
-                          height="20"
-                          viewBox="0 0 20 20"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M3 2H18V12.5L13 17.5H9.5L7 20H5V17.5H1V4L3 2ZM16 11.5V4H5V14H8V16.5L10.5 14H14.5L16 12.5V11.5ZM12.5 5.5H14V10H12.5V5.5ZM9.5 5.5H11V10H9.5V5.5Z"
-                            fill="currentColor"
-                          />
-                        </svg>
-                      </span>
-                      EXPERIMENTAL Login with Twitch
-                    </Button>
-                  </div>
+                  {key === "" && (
+                    <div className="mt-2">
+                      <Button
+                        className="twitchLoginButton"
+                        href={`https://id.twitch.tv/oauth2/authorize?client_id=${CLIENTID}&redirect_uri=https://${window.location.host}/EloOverlay&response_type=token&scope=user%3Aread%3Aemail`}
+                        aria-label="Login with Twitch"
+                      >
+                        <span className="twitchIcon" aria-hidden>
+                          <svg
+                            width="20"
+                            height="20"
+                            viewBox="0 0 20 20"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M3 2H18V12.5L13 17.5H9.5L7 20H5V17.5H1V4L3 2ZM16 11.5V4H5V14H8V16.5L10.5 14H14.5L16 12.5V11.5ZM12.5 5.5H14V10H12.5V5.5ZM9.5 5.5H11V10H9.5V5.5Z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        </span>
+                        Login with Twitch
+                      </Button>
+                    </div>
+                  )}
                 </Form.Group>
                 <Form.Group className="w-25 regionSelect">
                   <Form.Label>Region:</Form.Label>
