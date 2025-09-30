24ab264771a27fcbe9a89e472376e6a0e50bd400
deadofarceus <nikilanger@yahoo.de>
2025-09-30 18:28:55 +0200

    feat: Türkei 2v2

--- src/pages/TeamElo.tsx
+++ src/pages/TeamElo.tsx
@@ -6,30 +6,6 @@ import { Account } from "../types/LeagueTypes";
 import { GLOBALADDRESS } from "../types/WebsocketTypes";
 import Member, { MemberProps } from "../components/league/TeamElo/Member";
 
-const LoLProsIcons = new Map<string, string>([
-  // puuid -> icon
-  [
-    "7DRQQCX8ZrHCQ8yaTZuynsrB_YTmycTHsHNlBrz_-jCT4MXZfqMwlGTmyi5CSLFu1NgZ-Vv4wbb5jw",
-    "Pride",
-  ],
-  [
-    "3r7bHpDlTFV94uQPdvEHQYGkSZKaXCQ03g5bGYT4zEDt76zcCTkor6o64HkxwFVKlFk6GPZx3DoD7g",
-    "Santorin",
-  ],
-  [
-    "fwH10FImq6GWqr4AoH64g6ndA_6cJkt9L17WDB1Ld7qRcIST4UrixZV-TjUcCsunC9ffSfuhTGXsTQ",
-    "PowerOfEvil",
-  ],
-  [
-    "44mx3kKkVnD_UnqwbgIbJB0b3SiWBuqBVASmxakQp051njT1cEtCYPegbsH-M_g0ABKBCDHG-i8HzA",
-    "Broeki",
-  ],
-  [
-    "pecpyPDkyhd5o0vISeB4nrPitDQIL_9beuo7hZJBM7MYPTR7fnGuwOV1zVJtq4jBq1b1OBY_e7ilfA",
-    "LiLipp",
-  ],
-]);
-
 function TeamElo() {
   const [members, setMembers] = useState<MemberProps[]>([]);
   const iconMap = new Map<string, string>();
@@ -37,35 +13,27 @@ function TeamElo() {
 
   const fetchAllPlayers = async () => {
     try {
-      const response = await fetch(
-        "https://" + GLOBALADDRESS + "/lol/TeamElo/" + team
-      );
+      const response = await fetch("https://" + GLOBALADDRESS + "/lol/TeamElo/" + team);
       const data = await response.json();
 
       console.log(data);
 
       data.members.sort(
-        (a: any, b: any) =>
-          b.leagueEntrys[0].combinedLP - a.leagueEntrys[0].combinedLP
+        (a: any, b: any) => b.leagueEntrys[0].combinedLP - a.leagueEntrys[0].combinedLP
       );
 
-      const membersData: MemberProps[] = data.members.map(
-        (member: Account, index: number) => ({
-          streamer: member.name,
-          elo: {
-            rank: member.leagueEntrys[0].rank,
-            tier: member.leagueEntrys[0].tier,
-            lp: member.leagueEntrys[0].leaguePoints,
-          },
-          today:
-            member.leagueEntrys[0].combinedLP - member.leagueEntrys[0].lpStart,
-          matchhistory: member.leagueEntrys[0].lastMatches
-            .map((game) => game.win)
-            .slice(-5),
-          icon: `../teams/${team}/${LoLProsIcons.get(member.puuid)}.png`,
-          place: index + 1,
-        })
-      );
+      const membersData: MemberProps[] = data.members.map((member: Account, index: number) => ({
+        streamer: member.name,
+        elo: {
+          rank: member.leagueEntrys[0].rank,
+          tier: member.leagueEntrys[0].tier,
+          lp: member.leagueEntrys[0].leaguePoints,
+        },
+        today: member.leagueEntrys[0].combinedLP - member.leagueEntrys[0].lpStart,
+        matchhistory: member.leagueEntrys[0].lastMatches.map((game) => game.win).slice(-5),
+        icon: `../teams/${team}/${member.name}.png`,
+        place: index + 1,
+      }));
 
       setMembers(membersData);
     } catch (error) {
@@ -81,12 +49,8 @@ function TeamElo() {
 
   const { streamer } = useParams();
 
-  const ownClimber = members.find(
-    (c) => iconMap.get(c.streamer!) === streamer
-  )!;
-  const ownIndex = members.findIndex(
-    (c) => iconMap.get(c.streamer!) === streamer
-  );
+  const ownClimber = members.find((c) => iconMap.get(c.streamer!) === streamer)!;
+  const ownIndex = members.findIndex((c) => iconMap.get(c.streamer!) === streamer);
 
   console.log("ALL members", members);
 
