10c9fa34dcb1b124b23769aec3fc3f0bdd11ca59
deadofarceus <nikilanger@yahoo.de>
2025-11-19 22:59:50 +0100

    feat: dynamic version

--- src/types/Constants.ts
+++ src/types/Constants.ts
@@ -1,1 +1,9 @@
-export const CHAMPIMG_URL = "https://ddragon.leagueoflegends.com/cdn/15.20.1/img/champion/";
+getNewestPatch();
+export let CHAMPIMG_URL = "https://ddragon.leagueoflegends.com/cdn/15.23.1/img/champion/";
+function getNewestPatch() {
+  fetch("https://ddragon.leagueoflegends.com/api/versions.json")
+    .then((res) => res.json())
+    .then(
+      (data) => (CHAMPIMG_URL = `https://ddragon.leagueoflegends.com/cdn/${data[0]}/img/champion/`)
+    );
+}
