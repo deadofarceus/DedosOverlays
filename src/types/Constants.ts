5fbb88d1122970e2af451bf6de6e85fe69c66533
deadofarceus <nikilanger@yahoo.de>
2026-04-02 23:37:13 +0200

    april fools over

--- src/types/Constants.ts
+++ src/types/Constants.ts
@@ -1,10 +1,10 @@
-// getNewestPatch();
-// export let CHAMPIMG_URL = "https://ddragon.leagueoflegends.com/cdn/15.23.1/img/champion/";
-export let CHAMPIMG_URL = "../../champs/"; // april fools
-// function getNewestPatch() {
-//   fetch("https://ddragon.leagueoflegends.com/api/versions.json")
-//     .then((res) => res.json())
-//     .then(
-//       (data) => (CHAMPIMG_URL = `https://ddragon.leagueoflegends.com/cdn/${data[0]}/img/champion/`)
-//     );
-// }
+getNewestPatch();
+export let CHAMPIMG_URL = "https://ddragon.leagueoflegends.com/cdn/15.23.1/img/champion/";
+// export let CHAMPIMG_URL = "../../champs/"; // april fools
+function getNewestPatch() {
+  fetch("https://ddragon.leagueoflegends.com/api/versions.json")
+    .then((res) => res.json())
+    .then(
+      (data) => (CHAMPIMG_URL = `https://ddragon.leagueoflegends.com/cdn/${data[0]}/img/champion/`)
+    );
+}
