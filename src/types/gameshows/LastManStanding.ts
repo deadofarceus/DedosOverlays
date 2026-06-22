1bb7aa4c057ec0ab5bf6fb289696ce94c34bc589
deadofarceus <nikilanger@yahoo.de>
2026-06-22 16:36:18 +0200

    fix: randomized

--- src/types/gameshows/LastManStanding.ts
+++ src/types/gameshows/LastManStanding.ts
@@ -55,9 +55,9 @@ boards.push({
   size: 137,
   objects: [
     {
-      name: "Ashe",
+      name: "Fiora",
       revealed: false,
-      image: "Ashe_8",
+      image: "Fiora_4",
     },
     {
       name: "Akali",
@@ -65,35 +65,9 @@ boards.push({
       image: "Akali_14",
     },
     {
-      name: "Ekko",
-      revealed: false,
-      image: "Ekko_3",
-    },
-
-    {
-      name: "Fiora",
-      revealed: false,
-      image: "Fiora_4",
-    },
-    {
-      name: "Gangplank",
-      revealed: false,
-      image: "Gangplank_33",
-    },
-    {
-      name: "Irelia",
-      revealed: false,
-      image: "Irelia_16",
-    },
-    {
       name: "Jax",
       revealed: false,
       image: "Jax_33",
-    },
-    {
-      name: "Jhin",
-      revealed: false,
-      image: "Jhin_4",
     },
     {
       name: "Jinx",
@@ -101,29 +75,24 @@ boards.push({
       image: "Jinx_20",
     },
     {
-      name: "Katarina",
-      revealed: false,
-      image: "Katarina_9",
-    },
-    {
-      name: "Leona",
-      revealed: false,
-      image: "Leona_8",
-    },
-    {
-      name: "Lucian",
-      revealed: false,
-      image: "Lucian_6",
-    },
-    {
       name: "Mordekaiser",
       revealed: false,
       image: "Mordekaiser_6",
     },
     {
-      name: "Master Yi",
+      name: "Katarina",
       revealed: false,
-      image: "MasterYi_9",
+      image: "Katarina_9",
+    },
+    {
+      name: "Irelia",
+      revealed: false,
+      image: "Irelia_16",
+    },
+    {
+      name: "Gangplank",
+      revealed: false,
+      image: "Gangplank_33",
     },
     {
       name: "Naafiri",
@@ -131,14 +100,14 @@ boards.push({
       image: "Naafiri_11",
     },
     {
-      name: "Sejuani",
+      name: "Ashe",
       revealed: false,
-      image: "Sejuani_16",
+      image: "Ashe_8",
     },
     {
-      name: "Senna",
+      name: "Leona",
       revealed: false,
-      image: "Senna_16",
+      image: "Leona_8",
     },
     {
       name: "Sylas",
@@ -146,14 +115,39 @@ boards.push({
       image: "Sylas_13",
     },
     {
-      name: "Varus",
+      name: "Yasuo",
       revealed: false,
-      image: "Varus_16",
+      image: "Yasuo_2",
     },
     {
-      name: "Vayne",
+      name: "Lucian",
       revealed: false,
-      image: "Vayne_11",
+      image: "Lucian_6",
+    },
+    {
+      name: "Jhin",
+      revealed: false,
+      image: "Jhin_4",
+    },
+    {
+      name: "Master Yi",
+      revealed: false,
+      image: "MasterYi_9",
+    },
+    {
+      name: "Sejuani",
+      revealed: false,
+      image: "Sejuani_16",
+    },
+    {
+      name: "Zed",
+      revealed: false,
+      image: "Zed_3",
+    },
+    {
+      name: "Ekko",
+      revealed: false,
+      image: "Ekko_3",
     },
     {
       name: "Vi",
@@ -161,19 +155,24 @@ boards.push({
       image: "Vi_11",
     },
     {
+      name: "Varus",
+      revealed: false,
+      image: "Varus_16",
+    },
+    {
       name: "Warwick",
       revealed: false,
       image: "Warwick_16",
     },
     {
-      name: "Yasuo",
+      name: "Senna",
       revealed: false,
-      image: "Yasuo_2",
+      image: "Senna_16",
     },
     {
-      name: "Zed",
+      name: "Vayne",
       revealed: false,
-      image: "Zed_3",
+      image: "Vayne_11",
     },
   ],
 });
@@ -185,9 +184,20 @@ boards.push({
   size: 163,
   objects: [
     {
-      name: "Alistar",
+      name: "Ryze",
       revealed: false,
-      image: "Alistar_0",
+      image: "Ryze_0",
+    },
+
+    {
+      name: "Master Yi",
+      revealed: false,
+      image: "MasterYi_0",
+    },
+    {
+      name: "Fiddlesticks",
+      revealed: false,
+      image: "Fiddlesticks_0",
     },
     {
       name: "Annie",
@@ -195,60 +205,9 @@ boards.push({
       image: "Annie_0",
     },
     {
-      name: "Ashe",
-      revealed: false,
-      image: "Ashe_0",
-    },
-
-    {
-      name: "Fiddlesticks",
-      revealed: false,
-      image: "Fiddlesticks_0",
-    },
-    {
-      name: "Jax",
-      revealed: false,
-      image: "Jax_0",
-    },
-    {
       name: "Kayle",
       revealed: false,
       image: "Kayle_0",
-    },
-    {
-      name: "Master Yi",
-      revealed: false,
-      image: "MasterYi_0",
-    },
-    {
-      name: "Morgana",
-      revealed: false,
-      image: "Morgana_0",
-    },
-    {
-      name: "Nunu & Willump",
-      revealed: false,
-      image: "Nunu_0",
-    },
-    {
-      name: "Ryze",
-      revealed: false,
-      image: "Ryze_0",
-    },
-    {
-      name: "Sion",
-      revealed: false,
-      image: "Sion_0",
-    },
-    {
-      name: "Sivir",
-      revealed: false,
-      image: "Sivir_0",
-    },
-    {
-      name: "Soraka",
-      revealed: false,
-      image: "Soraka_0",
     },
     {
       name: "Teemo",
@@ -256,9 +215,14 @@ boards.push({
       image: "Teemo_0",
     },
     {
-      name: "Tristana",
+      name: "Morgana",
       revealed: false,
-      image: "Tristana_0",
+      image: "Morgana_0",
+    },
+    {
+      name: "Alistar",
+      revealed: false,
+      image: "Alistar_0",
     },
     {
       name: "Twisted Fate",
@@ -266,9 +230,44 @@ boards.push({
       image: "TwistedFate_0",
     },
     {
+      name: "Ashe",
+      revealed: false,
+      image: "Ashe_0",
+    },
+    {
+      name: "Sivir",
+      revealed: false,
+      image: "Sivir_0",
+    },
+    {
+      name: "Nunu & Willump",
+      revealed: false,
+      image: "Nunu_0",
+    },
+    {
+      name: "Sion",
+      revealed: false,
+      image: "Sion_0",
+    },
+    {
       name: "Warwick",
       revealed: false,
       image: "Warwick_0",
+    },
+    {
+      name: "Tristana",
+      revealed: false,
+      image: "Tristana_0",
+    },
+    {
+      name: "Soraka",
+      revealed: false,
+      image: "Soraka_0",
+    },
+    {
+      name: "Jax",
+      revealed: false,
+      image: "Jax_0",
     },
   ],
 });
@@ -279,152 +278,35 @@ boards.push({
   extra: "default",
   size: 137,
   objects: [
-    {
-      name: "Hextech Alistar",
-      revealed: false,
-      image: "Alistar_19",
-    },
-    {
-      name: "Hextech Amumu",
-      revealed: false,
-      image: "Amumu_23",
-    },
-    {
-      name: "Hextech Annie",
-      revealed: false,
-      image: "Annie_10",
-    },
-
-    {
-      name: "Dark Star Cho'Gath",
-      revealed: false,
-      image: "Chogath_7",
-    },
-    {
-      name: "Dreadnova Darius",
-      revealed: false,
-      image: "Darius_14",
-    },
-    {
-      name: "Lancer Zero Hecarim",
-      revealed: false,
-      image: "Hecarim_7",
-    },
-    {
-      name: "Hextech Jarvan IV",
-      revealed: false,
-      image: "JarvanIV_9",
-    },
-    {
-      name: "Hextech Kassadin",
-      revealed: false,
-      image: "Kassadin_14",
-    },
-    {
-      name: "Hextech Kog'Maw",
-      revealed: false,
-      image: "KogMaw_10",
-    },
-    {
-      name: "Hextech Malzahar",
-      revealed: false,
-      image: "Malzahar_7",
-    },
-    {
-      name: "Hextech Nocturne",
-      revealed: false,
-      image: "Nocturne_16",
-    },
-    {
-      name: "Hextech Poppy",
-      revealed: false,
-      image: "Poppy_15",
-    },
-    {
-      name: "Hextech Rammus",
-      revealed: false,
-      image: "Rammus_16",
-    },
-    {
-      name: "Hextech Renekton",
-      revealed: false,
-      image: "Renekton_17",
-    },
-    {
-      name: "Hextech Sejuani",
-      revealed: false,
-      image: "Sejuani_15",
-    },
-    {
-      name: "Hextech Swain",
-      revealed: false,
-      image: "Swain_11",
-    },
-    {
-      name: "Hextech Tristana",
-      revealed: false,
-      image: "Tristana_40",
-    },
-    {
-      name: "Soulstealer Vayne",
-      revealed: false,
-      image: "Vayne_10",
-    },
-    {
-      name: "Hextech Ziggs",
-      revealed: false,
-      image: "Ziggs_23",
-    },
-    {
-      name: "Hextech Ward",
-      revealed: false,
-      image: "HextechWard.png",
-    },
-    {
-      name: "Neo Pax Sivir",
-      revealed: false,
-      image: "Sivir_9",
-    },
-    {
-      name: "Dawnbringer Ward",
-      revealed: false,
-      image: "DawnbringerWard.png",
-    },
-    {
-      name: "Nightbringer Ward",
-      revealed: false,
-      image: "NightbringerWard.png",
-    },
-    {
-      name: "Lunar Wraith Caitlyn",
-      revealed: false,
-      image: "Caitlyn_10",
-    },
-    {
-      name: "Dragonwing Corki",
-      revealed: false,
-      image: "Corki_6",
-    },
-    {
-      name: "Warring Kingdoms Garen",
-      revealed: false,
-      image: "Garen_11",
-    },
-    {
-      name: "Warring Kingdoms Katarina",
-      revealed: false,
-      image: "Katarina_8",
-    },
-    {
-      name: "Guqin Sona",
-      revealed: false,
-      image: "Sona_4",
-    },
-    {
-      name: "Dragonblade Riven",
-      revealed: false,
-      image: "Riven_5",
-    },
+    { name: "Dragonwing Corki", revealed: false, image: "Corki_6" },
+    { name: "Hextech Swain", revealed: false, image: "Swain_11" },
+    { name: "Hextech Rammus", revealed: false, image: "Rammus_16" },
+    { name: "Dawnbringer Ward", revealed: false, image: "DawnbringerWard.png" },
+    { name: "Hextech Nocturne", revealed: false, image: "Nocturne_16" },
+    { name: "Hextech Annie", revealed: false, image: "Annie_10" },
+    { name: "Lunar Wraith Caitlyn", revealed: false, image: "Caitlyn_10" },
+    { name: "Hextech Kassadin", revealed: false, image: "Kassadin_14" },
+    { name: "Hextech Jarvan IV", revealed: false, image: "JarvanIV_9" },
+    { name: "Guqin Sona", revealed: false, image: "Sona_4" },
+    { name: "Hextech Amumu", revealed: false, image: "Amumu_23" },
+    { name: "Hextech Ziggs", revealed: false, image: "Ziggs_23" },
+    { name: "Soulstealer Vayne", revealed: false, image: "Vayne_10" },
+    { name: "Warring Kingdoms Garen", revealed: false, image: "Garen_11" },
+    { name: "Hextech Poppy", revealed: false, image: "Poppy_15" },
+    { name: "Nightbringer Ward", revealed: false, image: "NightbringerWard.png" },
+    { name: "Hextech Alistar", revealed: false, image: "Alistar_19" },
+    { name: "Dragonblade Riven", revealed: false, image: "Riven_5" },
+    { name: "Lancer Zero Hecarim", revealed: false, image: "Hecarim_7" },
+    { name: "Hextech Sejuani", revealed: false, image: "Sejuani_15" },
+    { name: "Dark Star Cho'Gath", revealed: false, image: "Chogath_7" },
+    { name: "Warring Kingdoms Katarina", revealed: false, image: "Katarina_8" },
+    { name: "Hextech Kog'Maw", revealed: false, image: "KogMaw_10" },
+    { name: "Neo Pax Sivir", revealed: false, image: "Sivir_9" },
+    { name: "Hextech Renekton", revealed: false, image: "Renekton_17" },
+    { name: "Hextech Tristana", revealed: false, image: "Tristana_40" },
+    { name: "Hextech Ward", revealed: false, image: "HextechWard.png" },
+    { name: "Dreadnova Darius", revealed: false, image: "Darius_14" },
+    { name: "Hextech Malzahar", revealed: false, image: "Malzahar_7" },
   ],
 });
 
@@ -434,97 +316,24 @@ boards.push({
   extra: "default",
   size: 164,
   objects: [
-    {
-      name: "Infinity Edge",
-      revealed: false,
-      image: "223031",
-    },
-    {
-      name: "Rabadon's Deathcap",
-      revealed: false,
-      image: "223089",
-    },
-    {
-      name: "Bloodthirster",
-      revealed: false,
-      image: "3072",
-    },
-
-    {
-      name: "Trinity Force",
-      revealed: false,
-      image: "3078",
-    },
-    {
-      name: "Overlord's Bloodmail",
-      revealed: false,
-      image: "447111",
-    },
-    {
-      name: "Lord Dominik's Regards",
-      revealed: false,
-      image: "223036",
-    },
-    {
-      name: "Ravenous Hydra",
-      revealed: false,
-      image: "223074",
-    },
-    {
-      name: "Titanic Hydra",
-      revealed: false,
-      image: "223748",
-    },
-    {
-      name: "Death's Dance",
-      revealed: false,
-      image: "226333",
-    },
-    {
-      name: "Stridebreaker",
-      revealed: false,
-      image: "226631",
-    },
-    {
-      name: "Zhonya's Hourglass",
-      revealed: false,
-      image: "3157",
-    },
-    {
-      name: "Bastionbreaker",
-      revealed: false,
-      image: "2520",
-    },
-    {
-      name: "Guardian Angel",
-      revealed: false,
-      image: "3026",
-    },
-    {
-      name: "Sterak's Gage",
-      revealed: false,
-      image: "3053",
-    },
-    {
-      name: "Mercurial Scimitar",
-      revealed: false,
-      image: "3139",
-    },
-    {
-      name: "Blade of The Ruined King",
-      revealed: false,
-      image: "3153",
-    },
-    {
-      name: "Shadowflame",
-      revealed: false,
-      image: "4645",
-    },
-    {
-      name: "Jak'Sho",
-      revealed: false,
-      image: "6665",
-    },
+    { name: "Zhonya's Hourglass", revealed: false, image: "3157" },
+    { name: "Shadowflame", revealed: false, image: "4645" },
+    { name: "Ravenous Hydra", revealed: false, image: "223074" },
+    { name: "Mercurial Scimitar", revealed: false, image: "3139" },
+    { name: "Infinity Edge", revealed: false, image: "223031" },
+    { name: "Bastionbreaker", revealed: false, image: "2520" },
+    { name: "Death's Dance", revealed: false, image: "226333" },
+    { name: "Blade of The Ruined King", revealed: false, image: "3153" },
+    { name: "Sterak's Gage", revealed: false, image: "3053" },
+    { name: "Trinity Force", revealed: false, image: "3078" },
+    { name: "Lord Dominik's Regards", revealed: false, image: "223036" },
+    { name: "Guardian Angel", revealed: false, image: "3026" },
+    { name: "Overlord's Bloodmail", revealed: false, image: "447111" },
+    { name: "Titanic Hydra", revealed: false, image: "223748" },
+    { name: "Stridebreaker", revealed: false, image: "226631" },
+    { name: "Rabadon's Deathcap", revealed: false, image: "223089" },
+    { name: "Jak'Sho", revealed: false, image: "6665" },
+    { name: "Bloodthirster", revealed: false, image: "3072" },
   ],
 });
 
@@ -534,117 +343,28 @@ boards.push({
   extra: "default",
   size: 136,
   objects: [
-    {
-      name: "Cowgirl",
-      revealed: false,
-      image: "MissFortune_1",
-    },
-    {
-      name: "Waterloo",
-      revealed: false,
-      image: "MissFortune_2",
-    },
-    {
-      name: "Crime City",
-      revealed: false,
-      image: "MissFortune_6",
-    },
-
-    {
-      name: "Arcade",
-      revealed: false,
-      image: "MissFortune_7",
-    },
-    {
-      name: "Captain",
-      revealed: false,
-      image: "MissFortune_8",
-    },
-    {
-      name: "Pool Party",
-      revealed: false,
-      image: "MissFortune_9",
-    },
-    {
-      name: "Star Guardian",
-      revealed: false,
-      image: "MissFortune_15",
-    },
-    {
-      name: "Gun Goddess",
-      revealed: false,
-      image: "MissFortune_16",
-    },
-    {
-      name: "Pajama Guardian",
-      revealed: false,
-      image: "MissFortune_17",
-    },
-    {
-      name: "Ruined",
-      revealed: false,
-      image: "MissFortune_21",
-    },
-    {
-      name: "Battle Bunny",
-      revealed: false,
-      image: "MissFortune_31",
-    },
-    {
-      name: "Broken Covenant",
-      revealed: false,
-      image: "MissFortune_40",
-    },
-    {
-      name: "Porcelain",
-      revealed: false,
-      image: "MissFortune_50",
-    },
-    {
-      name: "Battle Queen",
-      revealed: false,
-      image: "MissFortune_60",
-    },
-    {
-      name: "Secret Agent",
-      revealed: false,
-      image: "MissFortune_3",
-    },
-    {
-      name: "Candy Cane",
-      revealed: false,
-      image: "MissFortune_4",
-    },
-    {
-      name: "Road Warrior",
-      revealed: false,
-      image: "MissFortune_5",
-    },
-    {
-      name: "Bewitching",
-      revealed: false,
-      image: "MissFortune_18",
-    },
-    {
-      name: "Prestige Bewitching",
-      revealed: false,
-      image: "MissFortune_20",
-    },
-    {
-      name: "Admiral Battle Bunny",
-      revealed: false,
-      image: "MissFortune_32",
-    },
-    {
-      name: "Prtg. Bew. (2022)",
-      revealed: false,
-      image: "MissFortune_33",
-    },
-    {
-      name: "Prestige Broken Covenant",
-      revealed: false,
-      image: "MissFortune_41",
-    },
+    { name: "Prestige Broken Covenant", revealed: false, image: "MissFortune_41" },
+    { name: "Candy Cane", revealed: false, image: "MissFortune_4" },
+    { name: "Battle Queen", revealed: false, image: "MissFortune_60" },
+    { name: "Ruined", revealed: false, image: "MissFortune_21" },
+    { name: "Admiral Battle Bunny", revealed: false, image: "MissFortune_32" },
+    { name: "Cowgirl", revealed: false, image: "MissFortune_1" },
+    { name: "Pajama Guardian", revealed: false, image: "MissFortune_17" },
+    { name: "Porcelain", revealed: false, image: "MissFortune_50" },
+    { name: "Crime City", revealed: false, image: "MissFortune_6" },
+    { name: "Gun Goddess", revealed: false, image: "MissFortune_16" },
+    { name: "Bewitching", revealed: false, image: "MissFortune_18" },
+    { name: "Secret Agent", revealed: false, image: "MissFortune_3" },
+    { name: "Road Warrior", revealed: false, image: "MissFortune_5" },
+    { name: "Star Guardian", revealed: false, image: "MissFortune_15" },
+    { name: "Waterloo", revealed: false, image: "MissFortune_2" },
+    { name: "Broken Covenant", revealed: false, image: "MissFortune_40" },
+    { name: "Pool Party", revealed: false, image: "MissFortune_9" },
+    { name: "Prestige Bewitching", revealed: false, image: "MissFortune_20" },
+    { name: "Arcade", revealed: false, image: "MissFortune_7" },
+    { name: "Prtg. Bew. (2022)", revealed: false, image: "MissFortune_33" },
+    { name: "Battle Bunny", revealed: false, image: "MissFortune_31" },
+    { name: "Captain", revealed: false, image: "MissFortune_8" },
   ],
 });
 
